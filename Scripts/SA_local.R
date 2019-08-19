ae2127b5f502e0b4fd3ab70f2e3129c2c34ce67e
mhesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2019-08-19 14:42:37 +0200

    Update SA local

--- Scripts/SA_local.R
+++ Scripts/SA_local.R
@@ -13,160 +13,143 @@ pattern_1999_recon <- readr::read_rds("Data/Input/pattern_1999_reconstructed.rds
 
 source("Scripts/helper_functions.R")
 
-#### Create parameters ####
+#### Set SA parameters ####
+repetitions <- 5 # 50
+repetitions_hpc <- rep(x = 1, times = repetitions)
 
+#### Create parameters ####
 # increase parameters by 5% and 10%
 parameters_beech_inc_5 <- change_parameters(x = parameters_beech_default, 
-                                            change = 0.05)
+                                            change = 0.05) %>% 
+  rep(each = repetitions)
 
 parameters_beech_inc_10 <- change_parameters(x = parameters_beech_default, 
-                                             change = 0.1)
-
+                                             change = 0.1) %>% 
+  rep(each = repetitions)
 # decrease parameters by 5% and 10%
 parameters_beech_dec_5 <- change_parameters(x = parameters_beech_default, 
-                                            change = -0.05)
+                                            change = -0.05) %>% 
+  rep(each = repetitions)
 
 parameters_beech_dec_10 <- change_parameters(x = parameters_beech_default, 
-                                             change = -0.1)
+                                             change = -0.1) %>% 
+  rep(each = repetitions)
+
+parameters_beech_default <- list(parameters_beech_default) %>% 
+  rep(each = repetitions)
 
 #### Pre-processing of input data ####
 
-input_1999_beech <- tibble::as_tibble(pattern_1999_recon) %>% 
+data <- tibble::as_tibble(pattern_1999_recon) %>% 
   dplyr::filter(species == "Beech") %>% 
   dplyr::mutate(type = dplyr::case_when(type == "living" ~ "adult",
                                         type == "dead" ~ "dead"), 
                 species = "beech") %>%
   rabmp::prepare_data(x = "x", y = "y", species = "species", type = "type", dbh = "dbh")
 
-#### Set SA parameters ####
-
-repetitions <- 5 # 50
-repetitions_hpc <- rep(x = 1, times = repetitions)
-years <- 10 # 25
+#### Set model arguments ####
+plot_area <- pattern_1999_recon$window
+years <- 50 # 25
+seed_dispersal <- FALSE
+save_each <- 5
+verbose <- FALSE
 
 #### Default parameters ####
-
-# sa_default <- purrr::map(1:repetitions, function(x) run_model(data = input_1999_beech, 
-#                                                               parameters = parameters_beech_default,
-#                                                               plot_area = pattern_1999_recon$window, 
-#                                                               years = years))
-
 sa_default <- suppoRt::submit_to_cluster(wrap_run_model, 
-                                         x = repetitions_hpc, 
-                                         const = list(data = input_1999_beech, 
-                                                      parameters = parameters_beech_default,
-                                                      plot_area = pattern_1999_recon$window, 
+                                         parameters = parameters_beech_default,
+                                         const = list(data = data, 
+                                                      plot_area = plot_area, 
                                                       years = years, 
-                                                      save_each = 5,
-                                                      verbose = FALSE),
-                                         n_jobs = repetitions,
+                                                      seed_dispersal = seed_dispersal,
+                                                      save_each = save_each,
+                                                      verbose = verbose),
+                                         n_jobs = length(parameters_beech_default),
                                          template = list(job_name = "sa_default",
-                                                         walltime = "01:30:00", 
-                                                         queue = "fat",
-                                                         mem_cpu = "20GB"))
+                                                         walltime = "00:30:00", 
+                                                         queue = "medium"))
 
 suppoRt::save_rds(object = sa_default,
                   filename = "sa_default.rds",
                   path = "Data/Output/")
 
-rm(sa_default)
+# rm(sa_default)
 
 #### Increased parameters #### 
-
-# sa_increased_5 <- purrr::map(1:repetitions, function(x) run_model(data = input_1999_beech, 
-#                                                                   parameters = parameters_beech_inc_5,
-#                                                                   plot_area = pattern_1999_recon$window, 
-#                                                                   years = years))
-
 sa_increased_5 <- suppoRt::submit_to_cluster(wrap_run_model, 
-                                             x = repetitions_hpc, 
-                                             const = list(data = input_1999_beech, 
-                                                          parameters = parameters_beech_inc_5,
-                                                          plot_area = pattern_1999_recon$window, 
+                                             parameters = parameters_beech_inc_5,
+                                             const = list(data = data, 
+                                                          plot_area = plot_area, 
                                                           years = years, 
-                                                          verbose = FALSE),
-                                             n_jobs = repetitions,
+                                                          seed_dispersal = seed_dispersal,
+                                                          save_each = save_each,
+                                                          verbose = verbose),
+                                             n_jobs = length(parameters_beech_inc_5),
                                              template = list(job_name = "sa_inc_5",
-                                                             walltime = "01:30:00", 
-                                                             queue = "fat",
-                                                             mem_cpu = "20GB"))
+                                                             walltime = "00:30:00", 
+                                                             queue = "medium"))
 
 suppoRt::save_rds(object = sa_increased_5,
                   filename = "sa_increased_5.rds",
                   path = "Data/Output/")
 
-rm(sa_increased_5)
+# rm(sa_increased_5)
 
-# sa_increased_10 <- purrr::map(1:repetitions, function(x) run_model(data = input_1999_beech, 
-#                                                                    parameters = parameters_beech_inc_10,
-#                                                                    plot_area = pattern_1999_recon$window, 
-#                                                                    years = years))
 
 sa_increased_10 <- suppoRt::submit_to_cluster(wrap_run_model, 
-                                              x = repetitions_hpc, 
-                                              const = list(data = input_1999_beech, 
-                                                           parameters = parameters_beech_inc_10,
-                                                           plot_area = pattern_1999_recon$window, 
+                                              parameters = parameters_beech_inc_10,
+                                              const = list(data = data, 
+                                                           plot_area = plot_area, 
                                                            years = years, 
-                                                           verbose = FALSE),
-                                              n_jobs = repetitions,
+                                                           seed_dispersal = seed_dispersal,
+                                                           save_each = save_each,
+                                                           verbose = verbose),
+                                              n_jobs = length(parameters_beech_inc_10),
                                               template = list(job_name = "sa_inc_10",
-                                                              walltime = "01:30:00", 
-                                                              queue = "fat",
-                                                              mem_cpu = "20GB"))
+                                                              walltime = "00:30:00", 
+                                                              queue = "medium"))
 
 suppoRt::save_rds(object = sa_increased_10,
                   filename = "sa_increased_10.rds",
                   path = "Data/Output/")
 
-rm(sa_increased_10)
+# rm(sa_increased_10)
 
 #### Decreased parameters ####
-
-# sa_decreased_5 <- purrr::map(1:repetitions, function(x) run_model(data = input_1999_beech, 
-#                                                                   parameters = parameters_beech_dec_5,
-#                                                                   plot_area = pattern_1999_recon$window, 
-#                                                                   years = years))
-
 sa_decreased_5 <- suppoRt::submit_to_cluster(wrap_run_model, 
-                                             x = repetitions_hpc, 
-                                             const = list(data = input_1999_beech, 
-                                                          parameters = parameters_beech_dec_5,
-                                                          plot_area = pattern_1999_recon$window, 
+                                             parameters = parameters_beech_dec_5,
+                                             const = list(data = data, 
+                                                          plot_area = plot_area, 
                                                           years = years, 
-                                                          verbose = FALSE),
-                                             n_jobs = repetitions,
+                                                          seed_dispersal = seed_dispersal,
+                                                          save_each = save_each,
+                                                          verbose = verbose),
+                                             n_jobs = length(parameters_beech_dec_5),
                                              template = list(job_name = "sa_dec_5",
-                                                             walltime = "01:30:00", 
-                                                             queue = "fat",
-                                                             mem_cpu = "20GB"))
+                                                             walltime = "00:30:00", 
+                                                             queue = "medium"))
 
 suppoRt::save_rds(object = sa_decreased_5,
                   filename = "sa_decreased_5.rds",
                   path = "Data/Output/")
 
-rm(sa_decreased_5)
+# rm(sa_decreased_5)
 
-# sa_decreased_10 <- purrr::map(1:repetitions, function(x) run_model(data = input_1999_beech, 
-#                                                                    parameters = parameters_beech_dec_10,
-#                                                                    plot_area = pattern_1999_recon$window, 
-#                                                                    years = years))
 
 sa_decreased_10 <- suppoRt::submit_to_cluster(wrap_run_model, 
-                                              x = repetitions_hpc, 
-                                              const = list(data = input_1999_beech, 
-                                                           parameters = parameters_beech_dec_10,
-                                                           plot_area = pattern_1999_recon$window, 
+                                              parameters = parameters_beech_dec_10,
+                                              const = list(data = data, 
+                                                           plot_area = plot_area, 
                                                            years = years, 
-                                                           verbose = FALSE),
-                                              n_jobs = repetitions,
+                                                           seed_dispersal = seed_dispersal,
+                                                           save_each = save_each,
+                                                           verbose = verbose),
+                                              n_jobs = length(parameters_beech_dec_10),
                                               template = list(job_name = "sa_dec_10",
-                                                              walltime = "01:30:00", 
-                                                              queue = "fat",
-                                                              mem_cpu = "20GB"))
+                                                              walltime = "00:30:00", 
+                                                              queue = "medium"))
 
 suppoRt::save_rds(object = sa_decreased_10,
                   filename = "sa_decreased_10.rds",
                   path = "Data/Output/")
 
-rm(sa_decreased_10)
+# rm(sa_decreased_10)
