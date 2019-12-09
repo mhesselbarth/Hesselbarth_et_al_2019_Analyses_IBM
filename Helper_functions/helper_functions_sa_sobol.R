511e1de891105000c7cdef4f058c5b13645a1e64
mhesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2019-12-09 10:29:18 +0100

    Sobol abiotic for adults/saplings

--- Helper_functions/helper_functions_sa_sobol.R
+++ Helper_functions/helper_functions_sa_sobol.R
@@ -144,7 +144,11 @@ calc_sobol_indiv_abiotic <- function(x, data, parameters, abiotic, plot_area, ye
   
   output <- dplyr::filter(output, i == max(i), type != "dead")
   
-  nrow(output)
+  output_adult <- dplyr::filter(output, type == "adult")
+  
+  output_sapling <- dplyr::filter(output, type == "sapling")
+  
+  list(adult = nrow(output_adult), sapling = nrow(output_sapling))
 }
 
 calc_sobol_pcf_abiotic <- function(x, data, parameters, abiotic, plot_area, years, save_each) {
@@ -189,12 +193,33 @@ calc_sobol_pcf_abiotic <- function(x, data, parameters, abiotic, plot_area, year
   
   output <- dplyr::filter(output, i == max(i), type != "dead")
   
-  output_ppp <- spatstat::ppp(x = output$x, y = output$y, 
-                              window = plot_area)
+  output_adult <- dplyr::filter(output, type == "adult")
   
-  output_pcf <- spatstat::envelope(output_ppp, fun = "pcf", nsim = 199, verbose = FALSE,
-                                   funargs = list(divisor = "d", correction = "good", 
-                                                  r = seq(from = 0, to = 75, length.out = 525)))
+  output_sapling <- dplyr::filter(output, type == "sapling")
   
-  onpoint::summarise_envelope(output_pcf, seperated = FALSE, plot_result = FALSE)
+  output_ppp_adult <- spatstat::ppp(x = output_adult$x, y = output_adult$y,
+                                    window = plot_area)
+  
+  output_ppp_sapling <- spatstat::ppp(x = output_sapling$x, y = output_sapling$y,
+                                      window = plot_area)
+  
+  output_pcf_adult <- spatstat::envelope(output_ppp_adult, 
+                                         fun = "pcf", nsim = 199, verbose = FALSE,
+                                         funargs = list(divisor = "d", correction = "good",
+                                                        r = seq(from = 0, to = 75, 
+                                                                length.out = 525)))
+  
+  output_pcf_sapling <- spatstat::envelope(output_ppp_sapling, 
+                                           fun = "pcf", nsim = 199, verbose = FALSE,
+                                           funargs = list(divisor = "d", correction = "good",
+                                                          r = seq(from = 0, to = 75, 
+                                                                  length.out = 525)))
+  
+  envl_summarised_adult <- onpoint::summarise_envelope(output_pcf_adult, 
+                                                       seperated = FALSE, plot_result = FALSE)
+  
+  envl_summarised_sapling <- onpoint::summarise_envelope(output_pcf_sapling, 
+                                                         seperated = FALSE, plot_result = FALSE)
+  
+  list(adult = envl_summarised_adult, sapling = envl_summarised_sapling)
 }
