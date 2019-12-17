a86fcbeda7a056004a6ab6038b293a17c3cf9d35
mhesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2019-12-17 13:46:52 +0100

    Add mean distance k neighbour

--- Scripts_analysis/07_NND.R
+++ Scripts_analysis/07_NND.R
@@ -51,8 +51,46 @@ ggplot_nnd <- ggplot(data = nnd_overall_df) +
   theme_classic(base_size = base_size) + 
   theme(legend.position = "bottom")
 
-suppoRt::save_ggplot(plot = ggplot_nnd,
-                     filename = "nnd_overall_df.png",
+# distance to k neighbours
+nn_saplings <- purrr::map_df(1:10, function(x) {
+  
+  nn_dist <- nndist(saplings_1999_ppp, k = x)
+  
+  tibble::tibble(nn = x, mean = mean(nn_dist), sd = sd(nn_dist), type = "Sapling")
+})
+
+nn_adults <- purrr::map_df(1:10, function(x) {
+  
+  nn_dist <- nndist(adults_1999_ppp, k = x)
+  
+  tibble::tibble(nn = x, mean = mean(nn_dist), sd = sd(nn_dist), type = "Adult")
+})
+
+# combine to one df #
+nn_overall <- dplyr::bind_rows(nn_saplings, nn_adults) %>% 
+  dplyr::mutate(nn = factor(nn, ordered = TRUE), 
+                type = factor(type, levels = c("Sapling", "Adult")))
+
+# plot result #
+ggplot_nn <- ggplot(data = nn_overall) + 
+  geom_bar(aes(x = nn, y = mean, fill = type), 
+           stat = "identity", position = position_dodge()) +
+  geom_errorbar(aes(x = nn, ymin = mean - sd, ymax = mean + sd, group = type), 
+                width = 0.25, position = position_dodge(0.9)) +
+  scale_fill_manual(name = "", values = c("Sapling" = "#0D0887FF", 
+                                           "Adult" = "#CC4678FF")) + 
+  labs(x = expression(paste(italic(k),  " nearest neighbour")), 
+       y = "Distance [m]") +
+  theme_classic(base_size = base_size) + 
+  theme(legend.position = "bottom")
+
+ggplot_nnd_nn <- ggplot_nnd + ggplot_nn + 
+  patchwork::plot_layout(ncol = 2, nrow = 1,
+                         widths = c(0.5, 0.5), heights = c(0.5, 0.5)) + 
+  patchwork::plot_annotation(tag_levels = "a", tag_suffix = ")")
+
+suppoRt::save_ggplot(plot = ggplot_nnd_nn,
+                     filename = "ggplot_nnd_nn.png",
                      path = "Figures/Appendix",
                      dpi = dpi, 
                      width = width_full, height = height_small, units = units,
